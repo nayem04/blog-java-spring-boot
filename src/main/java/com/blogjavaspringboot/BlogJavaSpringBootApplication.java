ce433fb4c497a046ad52677e0ebb1a4ace272db6
Mohammad Nayem <sheikhmdnayem@gmail.com>
2025-01-07 12:33:36 +0600

    Initialized project

---
+++ src/main/java/com/blogjavaspringboot/BlogJavaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.blogjavaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogJavaSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogJavaSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blogjavaspringboot/BlogJavaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogjavaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogJavaSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
