c132e510dd3d6798be961040d2739344e01f1d98
Mohammad Nayem <sheikhmdnayem@gmail.com>
2025-01-07 14:56:25 +0600

    Added GlobalExceptionHandler class to handle exceptions globally

---
+++ src/main/java/com/blogjavaspringboot/common/utilities/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.blogjavaspringboot.common.utilities;
+
+import com.blogjavaspringboot.common.exceptions.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<String> handleBookNotFoundException(NotFoundException notFoundException) {
+        return new ResponseEntity<>(notFoundException.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception exception) {
+        return new ResponseEntity<>("An unexpected error occurred: " + exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
