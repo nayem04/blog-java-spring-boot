7f870f8e8ecf348992a4cea3417f1fe449ddbebc
Mohammad Nayem <sheikhmdnayem@gmail.com>
2025-01-07 20:26:11 +0600

    Added a exception handler method to handle exceptions caused by object properties' invalid values

--- src/main/java/com/blogjavaspringboot/common/utilities/GlobalExceptionHandler.java
+++ src/main/java/com/blogjavaspringboot/common/utilities/GlobalExceptionHandler.java
@@ -3,14 +3,28 @@ package com.blogjavaspringboot.common.utilities;
 import com.blogjavaspringboot.common.exceptions.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<String> handleBookNotFoundException(NotFoundException notFoundException) {
         return new ResponseEntity<>(notFoundException.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Exception.class)
