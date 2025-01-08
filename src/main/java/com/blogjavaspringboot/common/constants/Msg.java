d263cd9d734d6e9094dfe5e9b1cc9edea00e4840
Mohammad Nayem <sheikhmdnayem@gmail.com>
2025-01-08 17:47:49 +0600

    Added constants for generating messages

--- src/main/java/com/blogjavaspringboot/common/constants/Msg.java
+++ src/main/java/com/blogjavaspringboot/common/constants/Msg.java
@@ -2,14 +2,14 @@ package com.blogjavaspringboot.common.constants;
 
 public final class Msg {
     public final static class Entity {
-        public final static String ENTITY_NAME = "";
+        public final static String USER = "User";
     }
 
     public final static class Response {
-        public final static String SEARCH = " Searched Successfully";
-        public final static String FIND = " Found Successfully";
-        public final static String CREATE = " Created Successfully";
-        public final static String UPDATE = " Updated Successfully";
-        public final static String DELETE = " Deleted Successfully";
+        public final static String SEARCH = " Searched Successfully.";
+        public final static String FIND = " Found Successfully.";
+        public final static String CREATE = " Created Successfully.";
+        public final static String UPDATE = " Updated Successfully.";
+        public final static String DELETE = " Deleted Successfully.";
     }
 }
