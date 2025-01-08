eea7e43d492075cf1a9a51807b12ece8a03bdd89
Mohammad Nayem <sheikhmdnayem@gmail.com>
2025-01-08 18:29:57 +0600

    Added UserDto class

---
+++ src/main/java/com/blogjavaspringboot/domain/dtos/UserDto.java
@@ -0,0 +1,54 @@
+package com.blogjavaspringboot.domain.dtos;
+
+import com.blogjavaspringboot.common.base.BaseDto;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public class UserDto extends BaseDto {
+    @NotBlank(message = "First Name is required.")
+    @JsonProperty(value = "first_name")
+    private String firstName;
+
+    @JsonProperty(value = "last_name")
+    private String lastName;
+
+    @Email(message = "Email is invalid.")
+    @JsonProperty(value = "email")
+    private String email;
+
+    @JsonProperty(value = "phone_number")
+    private String phoneNumber;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
