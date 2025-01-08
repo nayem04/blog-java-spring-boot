364d5460f7ddcff3e156e8e5ead3c4db97cc687c
Mohammad Nayem <sheikhmdnayem@gmail.com>
2025-01-08 14:40:20 +0600

    Set timezone to Dhaka

--- src/main/java/com/blogjavaspringboot/BlogJavaSpringBootApplication.java
+++ src/main/java/com/blogjavaspringboot/BlogJavaSpringBootApplication.java
@@ -1,7 +1,10 @@
 package com.blogjavaspringboot;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class BlogJavaSpringBootApplication {
@@ -10,4 +13,10 @@ public class BlogJavaSpringBootApplication {
         SpringApplication.run(BlogJavaSpringBootApplication.class, args);
     }
 
+    //@PostConstruct is used to mark a method that should be run after the bean is initialized.
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("GMT+06:00"));
+        System.out.println("==== Timezone set to Dhaka ====");
+    }
 }
