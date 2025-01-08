6e0bf046ee7f97cd670b50d7b9024fd02fdd9085
Mohammad Nayem <sheikhmdnayem@gmail.com>
2025-01-08 17:50:29 +0600

    Added constants of routes for user APIs

--- src/main/java/com/blogjavaspringboot/common/routing/Router.java
+++ src/main/java/com/blogjavaspringboot/common/routing/Router.java
@@ -2,4 +2,12 @@ package com.blogjavaspringboot.common.routing;
 
 public final class Router {
     private static final String ROOT = "api/";
+
+    public static final class User {
+        public static final String GET_USERS = ROOT + "users";
+        public static final String GET_USER = ROOT + "users/{id}";
+        public static final String CREATE_USER = ROOT + "users";
+        public static final String UPDATE_USER = ROOT + "users/{id}";
+        public static final String DELETE_USER = ROOT + "users/{id}";
+    }
 }
