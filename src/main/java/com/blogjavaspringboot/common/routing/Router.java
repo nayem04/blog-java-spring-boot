31ff92349e7e9053107fe87881d537261831817c
Mohammad Nayem <sheikhmdnayem@gmail.com>
2025-01-08 19:59:56 +0600

    Added constants of routes for Post APIs

--- src/main/java/com/blogjavaspringboot/common/routing/Router.java
+++ src/main/java/com/blogjavaspringboot/common/routing/Router.java
@@ -10,4 +10,12 @@ public final class Router {
         public static final String UPDATE_USER = ROOT + "users/{id}";
         public static final String DELETE_USER = ROOT + "users/{id}";
     }
+
+    public static final class Post {
+        public static final String GET_POSTS = ROOT + "posts";
+        public static final String GET_POST = ROOT + "posts/{id}";
+        public static final String CREATE_POST = ROOT + "posts";
+        public static final String UPDATE_POST = ROOT + "posts/{id}";
+        public static final String DELETE_POST = ROOT + "posts/{id}";
+    }
 }
