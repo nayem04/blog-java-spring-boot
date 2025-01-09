88f24c5572c388c307fb9be73769cc4e12af1440
Mohammad Nayem <sheikhmdnayem@gmail.com>
2025-01-09 11:41:49 +0600

    Added a exception handler method to handle database related exceptions

--- src/main/java/com/blogjavaspringboot/common/utilities/GlobalExceptionHandler.java
+++ src/main/java/com/blogjavaspringboot/common/utilities/GlobalExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.blogjavaspringboot.common.utilities;
 
 import com.blogjavaspringboot.common.exceptions.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import java.util.HashMap;
 import java.util.Map;
 
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(NotFoundException.class)
@@ -18,6 +22,7 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(notFoundException.getMessage(), HttpStatus.NOT_FOUND);
     }
 
+    //Bean Validation Related Exceptions Handler
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
@@ -27,6 +32,17 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
+    //Database Related Exceptions Handler
+    /*Log the exception to capture details for debugging while ensuring sensitive data is not exposed in the response.*/
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
+        Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+        logger.error("Database error occurred: {}", ex.getMessage(), ex);
+        String message = "An error occurred while processing your request. Please check your input and try again.";
+        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+    }
+
+    //Generic Exceptions Handler
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(Exception exception) {
         return new ResponseEntity<>("An unexpected error occurred: " + exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
