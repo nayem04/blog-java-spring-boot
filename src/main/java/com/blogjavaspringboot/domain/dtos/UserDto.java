1153eb16361a344d01422fcd9f9d7f0cd2d2730a
Mohammad Nayem <sheikhmdnayem@gmail.com>
2025-01-09 12:11:01 +0600

    Added a vlidation for phone number in UserDto class

--- src/main/java/com/blogjavaspringboot/domain/dtos/UserDto.java
+++ src/main/java/com/blogjavaspringboot/domain/dtos/UserDto.java
@@ -4,6 +4,7 @@ import com.blogjavaspringboot.common.base.BaseDto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public class UserDto extends BaseDto {
     @NotBlank(message = "First Name is required.")
@@ -17,6 +18,7 @@ public class UserDto extends BaseDto {
     @JsonProperty(value = "email")
     private String email;
 
+    @Pattern(message = "Invalid phone number.", regexp = "^(\\+8801|01)[3-9][0-9]{8}$")
     @JsonProperty(value = "phone_number")
     private String phoneNumber;
 
